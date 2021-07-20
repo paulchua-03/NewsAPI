d409541c9b1285a6a386a9c3dc47943288607b3d
paulchua-03 <paulchua.pc3@gmail.com>
2021-07-20 20:45:21 +0800

    Initial Commit

---
+++ NewsAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAPI
+//
+//  Created by Paul Angelo Chua on 7/20/21.
+//  Copyright © 2021 Paul Angelo Chua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
